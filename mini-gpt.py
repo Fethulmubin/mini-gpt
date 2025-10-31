d070e024b59072d619af2908faa03fdb5291fa6f
fethulmubin <fetihul@gmail.com>
2025-10-31 23:33:43 +0300

    quantum qiskit implementation

--- mini-gpt.py
+++ mini-gpt.py
@@ -120,7 +120,6 @@ class Block(nn.Module):
     """ Transformer block: communication followed by computation """
 
     def __init__(self, n_embd, n_head):
-        # n_embd: embedding dimension, n_head: the number of heads we'd like
         super().__init__()
         head_size = n_embd // n_head
         self.sa = MultiHeadAttention(n_head, head_size)
